cc68f9e33476f453bcab983487e6cd02bf679d09
Mandar Dhake <100260019+mkdhake2000@users.noreply.github.com>
2023-08-17 11:41:06 +0530

    Add files via upload

---
+++ assign1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+jupyter nbconvert --to notebook --execute Q1.ipynb
+jupyter nbconvert --to notebook --execute Q2.ipynb
+jupyter nbconvert --to notebook --execute Q3.ipynb
+jupyter nbconvert --to notebook --execute Q4.ipynb
+jupyter nbconvert --to notebook --execute Q5.ipynb
+jupyter nbconvert --to notebook --execute Q6.ipynb
+jupyter nbconvert --to notebook --execute Q7.ipynb
+jupyter nbconvert --to notebook --execute Q8.ipynb
+jupyter nbconvert --to notebook --execute Q9.ipynb
